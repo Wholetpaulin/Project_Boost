94466bfa4cdba0140da1436ca9b81135999835f2
Robert Paul Trettenero <paul.trettenero@petrocloud.com>
2018-03-21 18:28:17 -0500

    Did inital work on reading input and setting up rocket ship models

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+    
+    private void ProcessInput()
+    {
+        //thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("space pressed");
+        }
+        //rotate left
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("D pressed");
+        }
+        //rotate right
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("A pressed");
+        }
+    }
+}
