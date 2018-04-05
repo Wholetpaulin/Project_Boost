839fb529a6fdd3e7cd9a2a94e685b1035ac2a20d
Robert Paul Trettenero <paul.trettenero@petrocloud.com>
2018-04-05 17:39:36 -0500

    Worked out weird collision kinks, Fiddled with sound a bit

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
@@ -8,6 +9,13 @@ public class Rocket : MonoBehaviour {
     AudioSource audio;
     [SerializeField] float rcsThrust = 250f;
     [SerializeField] float mainThrust = 10f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explode;
+    [SerializeField] AudioClip startChime;
+
+
+    enum State { Alive, Dying, Transcending }
+    [SerializeField] State state = State.Alive;
 
 
     // Use this for initialization
@@ -18,24 +26,48 @@ public class Rocket : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Thrust();
-        Rotate();
+        // todo somewhere stop sound on death
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; }
         switch(collision.gameObject.tag)
-        {
+        { 
             case "Friendly":
                 //do nothing
                 break;
+            case "Finish":
+                print("Hit finish");
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);    // This coroutine waits 1 second before executing method
+                break;
             default:
                 print("You're dead as shit!");
+                state = State.Dying;
+                Invoke("ResetScene", 1f);
                 break;
         }
     }
 
-    private void Rotate()
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void ResetScene()
+    {
+        audio.PlayOneShot(startChime);
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true;    // take manual control of rotation
         float rotationThisFrame = rcsThrust * Time.deltaTime;
@@ -53,20 +85,25 @@ public class Rocket : MonoBehaviour {
 
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         //thrust
         if (Input.GetKey(KeyCode.Space))
         {
-            if (!audio.isPlaying)
-            {
-                audio.Play();
-            }
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            ApplyThrust();
         }
         else
         {
             audio.Pause();
         }
+    }
+
+    private void ApplyThrust()
+    {
+        if (!audio.isPlaying)
+        {
+            audio.PlayOneShot(mainEngine);
+        }
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
     }
 }
